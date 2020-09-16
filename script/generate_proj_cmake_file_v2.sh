5d31b2c0368c5af531098b4eca7f16cff6a8d1c3
rjf12138 <3161173206@qq.com>
2020-09-16 23:17:28 +0800

    2020年 09月 16日 星期三 23:16:46 CST

--- script/generate_proj_cmake_file_v2.sh
+++ script/generate_proj_cmake_file_v2.sh
@@ -19,6 +19,38 @@ COMPILE_CONFIG_PATH=$PROJ_PROJECT_PATH/.proj_config/compile_config.json
 JSON_PARSE=$PROJ_PROJECT_PATH/.proj_config/parse_json
 cd $PROJ_PROJECT_PATH
 
+###########################################################################
+# JSON 解析封装函数
+# # 只能打印根节点下级数组的全部对象
+function print_obj_val()
+{
+echo '<< EOF
+print $1
+quit
+EOF' | $JSON_PARSE $COMPILE_CONFIG | tr -d "\n"
+}
+
+function print_array_val()
+{
+echo '<< EOF
+cd $1
+print $2
+quit
+EOF' | $JSON_PARSE $COMPILE_CONFIG | tr -d "\n"
+}
+
+function print_array_all()
+{
+echo '<< EOF
+cd $1
+print $2
+quit
+EOF' | $JSON_PARSE $COMPILE_CONFIG | tr -d "\n"
+}
+
+###########################################################################
+
+
 if [ -z `$JSON_PARSE $COMPILE_CONFIG_PATH 项目md5编码`];then
     compile_method=$1  # debug or release
     generate_file_type="exe" # static_lib/share_lib/exe
@@ -33,4 +65,15 @@ if [ -z `$JSON_PARSE $COMPILE_CONFIG_PATH 项目md5编码`];then
     share_lib_file_list=`cd ./lib/$compile_method/; ls *.so | tr -s " " | awk '{ORS=" "; print $1}'`
 else
     #从json配置文件中读取
+    compile_method=`print_obj_val 编译方式`
+    generate_file_type=`print_obj_val 生成文件类型`
+    compiler=`print_obj_val 编译器`
+    release_compile_option=`print_obj_val release版编译选项`
+    debug_compile_option=`print_obj_val debug版编译选项`
+    head_file_path=`print_arr_all 头文件路径`
+    lib_file_path=`print_arr_all 库文件路径`
+    exe_file_list=`ls ./main/ | tr -s \" \" | awk '{ORS=\" \"; print $1}'`
+    src_file_list=`ls ./src/ | tr -s \" \" | awk '{ORS=\" \"; print $1}'`
+    static_lib_file_list=`cd ./lib/$compile_method/; ls *.a | tr -s " " | awk '{ORS=" "; print $1}'`
+    share_lib_file_list=`cd ./lib/$compile_method/; ls *.so | tr -s " " | awk '{ORS=" "; print $1}'`
 fi

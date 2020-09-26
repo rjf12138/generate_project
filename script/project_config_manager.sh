fb4516fb8906546985763823372118508341c6c5
rjf12138 <3161173206@qq.com>
2020-09-26 23:20:28 +0800

    2020年 09月 26日 星期六 23:20:22 CST

--- script/project_config_manager.sh
+++ script/project_config_manager.sh
@@ -68,8 +68,9 @@ function config_project_config()
 	generate_file_type_list=`print_arr_all 可生成文件类型`
 	current_program_entry_file=`print_obj_val 当前程序入口文件`
 	program_entry_file_list=`print_arr_all 程序入口文件列表`
-	header_file_path_list=`print_arr_all 头文件目录列表`
+	header_dir_path_list=`print_arr_all 头文件目录列表`
 	static_lib_dir_list=`print_arr_all 静态库目录列表`
+	static_lib_list=`print_arr_all 静态库列表`
 	src_file_dir_list=`print_arr_all 源文件目录列表`
 	project_uuid=`print_obj_val 项目UUID`
 
@@ -80,10 +81,10 @@ function config_project_config()
 								">当前编译器: " "$current_compiler" ">编译方式: " "$compile_method" ">debug编译选项: " "$debug_compile_option"
 								">release编译选项: " "$release_compile_option" ">当前生成文件类型: " "$current_generate_file_type"
 								">当前程序入口文件: "  "$current_program_entry_file" ">头文件目录列表" "查看"
-								">静态库目录列表" "查看" ">源文件目录列表" "查看")
+								">静态库目录列表" "查看" ">静态库列表" "查看" ">源文件目录列表" "查看")
 
 		project_config_num=`expr ${#project_config_info[*]} / 2`
-		OPTION=$(whiptail --title "Menu Dialog" --menu "项目配置" 20 50 $project_config_num "${project_config_info[@]}"  3>&1 1>&2 2>&3)
+		OPTION=$(whiptail --title "项目配置" --menu "项目配置" 20 50 $project_config_num "${project_config_info[@]}"  3>&1 1>&2 2>&3)
 
 		exitstatus=$?
 		if [ $exitstatus != 0 ]; then
@@ -95,218 +96,330 @@ function config_project_config()
 			"*项目名称: "|"*项目UUID: "|"*项目路径: ")
 				continue
 				;;
+			">静态库列表")
+				while true
+				do
+					OPTION=$(whiptail --title "Menu Dialog" --menu "静态库" 15 60 3 \
+					"1" "添加静态库" \
+					"2" "删除静态库" \
+					"3" "查看静态库列表" \
+					3>&1 1>&2 2>&3)
+
+					exitstatus=$?
+					if [ $exitstatus != 0 ]; then
+						echo "You chose Cancel."
+						break
+					fi
+			
+					case $OPTION in
+						"1")
+							new_lib=$(whiptail --title "添加静态库" --inputbox "" 10 60 3>&1 1>&2 2>&3)
+
+							exitstatus=$?
+							if [ $exitstatus == 0 ]; then
+								static_lib_list=$static_lib_list" "$new_lib
+							fi
+						;;
+						"2")
+							while true 
+							do
+								unset static_libs
+								for static_lib in $static_lib_list
+								do
+									static_libs_num=`expr ${#static_libs[*]} / 2 + 1`
+									static_libs[${#static_libs[*]}]=$static_libs_num
+									static_libs[${#static_libs[*]}]=$static_lib
+								done
+
+								static_libs_num=`expr ${#static_libs[*]} / 2`
+								OPTION=$(whiptail --title "Menu Dialog" --menu "删除源文件目录" 15 60 $static_libs_num "${static_libs[@]}" 3>&1 1>&2 2>&3)
+
+								exitstatus=$?
+								if [ $exitstatus != 0 ]; then
+									echo "You chose Cancel."
+									break
+								fi
+
+								index=$[$OPTION * 2 - 1]
+								del_static_lib=${static_libs[$index]}
+
+								if (whiptail --title "Yes/No Box" --yesno "确定要移除 $del_static_lib" 10 60) then
+									static_lib_list=`echo $static_lib_list | sed "s#$del_static_lib##g"`
+								else
+									echo "You chose No. Exit status was $?."
+								fi
+							done 
+							;;
+						"3")
+							while true
+							do
+								unset static_libs
+								for static_lib in $static_lib_list
+								do
+									static_libs_num=`expr ${#static_libs[*]} / 2 + 1`
+									static_libs[${#static_libs[*]}]=$static_libs_num
+									static_libs[${#static_libs[*]}]=$static_lib
+								done
+
+								static_libs_num=`expr ${#static_libs[*]} / 2`
+								OPTION=$(whiptail --title "Menu Dialog" --menu "删除源文件目录" 15 60 $static_libs_num "${static_libs[@]}" 3>&1 1>&2 2>&3)
+
+								exitstatus=$?
+								if [ $exitstatus != 0 ]; then
+									echo "You chose Cancel."
+									break
+								fi
+							done
+						;;
+					esac
+				done
+			;;
 			">源文件目录列表")
-				OPTION=$(whiptail --title "Menu Dialog" --menu "源文件目录" 15 60 2 \
-				"1" "添加源文件目录" \
-				"2" "删除源文件目录" \
-				"3" "源文件目录列表" \
-				3>&1 1>&2 2>&3)
+				while true
+				do
+					OPTION=$(whiptail --title "Menu Dialog" --menu "源文件目录" 15 60 3 \
+					"1" "添加源文件目录" \
+					"2" "删除源文件目录" \
+					"3" "源文件目录列表" \
+					3>&1 1>&2 2>&3)
 
-				exitstatus=$?
-				if [ $exitstatus != 0 ]; then
-					echo "You chose Cancel."
-					break
-				fi
-		
-				case $OPTION in
-					"添加源文件目录")
-						new_dir=$(whiptail --title "添加源文件目录" --inputbox "" 10 60 3>&1 1>&2 2>&3)
+					exitstatus=$?
+					if [ $exitstatus != 0 ]; then
+						echo "You chose Cancel."
+						break
+					fi
+			
+					case $OPTION in
+						"1")
+							new_dir=$(whiptail --title "添加源文件目录" --inputbox "" 10 60 3>&1 1>&2 2>&3)
 
-						exitstatus=$?
-						if [ $exitstatus != 0 ]; then
-							echo "You chose Cancel."
-							break
-						fi
-						
-						src_file_dir_list=$src_file_dir_list" "$new_dir
+							exitstatus=$?
+							if [ $exitstatus == 0 ]; then
+								src_file_dir_list=$src_file_dir_list" "$new_dir
+							fi
 						;;
-					"删除源文件目录")
-						for src_file in $src_file_dir_list
-						do
-							src_files_num=`expr ${#choose_compiler[*]} / 2`
-							src_files[${#src_files[*]}]=$src_files_num
-							src_files[${#src_files[*]}]=$src_file
-							option_state[${#option_state[*]}]=OFF
-						done
+						"2")
+							while true 
+							do
+								unset src_files_dirs
+								for src_file_dir in $src_file_dir_list
+								do
+									src_files_dirs_num=`expr ${#src_files_dirs[*]} / 2 + 1`
+									src_files_dirs[${#src_files_dirs[*]}]=$src_files_dirs_num
+									src_files_dirs[${#src_files_dirs[*]}]=$src_file_dir
+								done
 
-						src_files_num=`expr ${#src_files[*]} / 2`
-						OPTION=$(whiptail --title "Menu Dialog" --checklist "删除源文件目录" 15 60 $src_files_num "${src_files[@]}" ${option_state[@]} 3>&1 1>&2 2>&3)
+								src_files_dirs_num=`expr ${#src_files_dirs[*]} / 2`
+								OPTION=$(whiptail --title "Menu Dialog" --menu "删除源文件目录" 15 60 $src_files_dirs_num "${src_files_dirs[@]}" 3>&1 1>&2 2>&3)
 
-						exitstatus=$?
-						if [ $exitstatus != 0 ]; then
-							echo "You chose Cancel."
-							break
-						fi
+								exitstatus=$?
+								if [ $exitstatus != 0 ]; then
+									echo "You chose Cancel."
+									break
+								fi
 
-						for del_file in $OPTION
-						do
-							src_file_dir_list=`echo $src_file_dir_list | sed "s/$del_file//g"`
-						done
+								index=$[$OPTION * 2 - 1]
+								del_src_dir=${src_files_dirs[$index]}
+
+								if (whiptail --title "Yes/No Box" --yesno "确定要移除 $del_src_dir" 10 60) then
+									src_file_dir_list=`echo $src_file_dir_list | sed "s#$del_src_dir##g"`
+								else
+									echo "You chose No. Exit status was $?."
+								fi
+							done 
+							;;
+						"3")
+							while true
+							do
+								unset src_files_dirs
+								for src_file_dir in $src_file_dir_list
+								do
+									src_files_dirs_num=`expr ${#src_files_dirs[*]} / 2 + 1`
+									src_files_dirs[${#src_files_dirs[*]}]=$src_files_dirs_num
+									src_files_dirs[${#src_files_dirs[*]}]=$src_file_dir
+								done
+
+								src_files_dirs_num=`expr ${#src_files_dirs[*]} / 2`
+								OPTION=$(whiptail --title "Menu Dialog" --menu "删除源文件目录" 15 60 $src_files_dirs_num "${src_files_dirs[@]}"3>&1 1>&2 2>&3)
+
+								exitstatus=$?
+								if [ $exitstatus != 0 ]; then
+									echo "You chose Cancel."
+									break
+								fi
+							done
 						;;
-					"源文件目录列表")
-						for src_file in $src_file_dir_list
-						do
-							src_files_num=`expr ${#choose_compiler[*]} / 2`
-							src_files[${#src_files[*]}]=$src_files_num
-							src_files[${#src_files[*]}]=$src_file
-							option_state[${#option_state[*]}]=OFF
-						done
-
-						src_files_num=`expr ${#src_files[*]} / 2`
-						OPTION=$(whiptail --title "Menu Dialog" --menu "删除源文件目录" 15 60 $src_files_num "${src_files[@]}" ${option_state[@]} 3>&1 1>&2 2>&3)
-
-						exitstatus=$?
-						if [ $exitstatus != 0 ]; then
-							echo "You chose Cancel."
-							break
-						fi
-						;;
-				esac
+					esac
+				done
 				;;
 			">静态库目录列表")
-				OPTION=$(whiptail --title "Menu Dialog" --menu "静态库目录" 15 60 2 \
-				"1" "添加静态库目录" \
-				"2" "删除静态库目录" \
-				"3" "静态库目录列表" 3>&1 1>&2 2>&3)
+				while true
+				do
+					OPTION=$(whiptail --title "Menu Dialog" --menu "静态库目录" 15 60 3 \
+					"1" "添加静态库目录" \
+					"2" "删除静态库目录" \
+					"3" "静态库目录列表" 3>&1 1>&2 2>&3)
 
-				exitstatus=$?
-				if [ $exitstatus != 0 ]; then
-					echo "You chose Cancel."
-					break
-				fi
+					exitstatus=$?
+					if [ $exitstatus != 0 ]; then
+						echo "You chose Cancel."
+						break
+					fi
 
-				case $OPTION in
-					"添加静态库目录")
-						new_static_lib_dir=$(whiptail --title "添加静态库目录" --inputbox "" 10 60 3>&1 1>&2 2>&3)
+					case $OPTION in
+						"1")
+							new_static_lib_dir=$(whiptail --title "添加静态库目录" --inputbox "添加静态库目录" 10 60 3>&1 1>&2 2>&3)
 
-						exitstatus=$?
-						if [ $exitstatus != 0 ]; then
-							echo "You chose Cancel."
-							break
-						fi
-						
-						static_lib_dir_list=$static_lib_dir_list" "$new_static_lib_dir
+							exitstatus=$?
+							if [ $exitstatus == 0 ]; then
+								static_lib_dir_list=$static_lib_dir_list" "$new_static_lib_dir
+							fi
+							;;
+						"2")
+							while true 
+							do
+								unset static_lib_dirs
+								for static_lib_dir in $static_lib_dir_list
+								do
+									static_lib_dirs_num=`expr ${#static_lib_dirs[*]} / 2 + 1`
+									static_lib_dirs[${#static_lib_dirs[*]}]=$static_lib_dirs_num
+									static_lib_dirs[${#static_lib_dirs[*]}]=$static_lib_dir
+								done
+
+								static_lib_dirs_num=`expr ${#static_lib_dirs[*]} / 2`
+								OPTION=$(whiptail --title "Menu Dialog" --menu "删除头文件目录" 15 60 $static_lib_dirs_num "${static_lib_dirs[@]}" 3>&1 1>&2 2>&3)
+
+								exitstatus=$?
+								if [ $exitstatus != 0 ]; then
+									echo "You chose Cancel."
+									break
+								fi
+
+								index=$[$OPTION * 2 - 1]
+								del_lib_dir=${static_lib_dirs[$index]}
+
+								if (whiptail --title "Yes/No Box" --yesno "确定要移除 $del_lib_dir" 10 60) then
+									static_lib_dir_list=`echo $static_lib_dir_list | sed "s#$del_lib_dir##g"`
+								else
+									echo "You chose No. Exit status was $?."
+								fi
+							done 
+							;;
+						"3")
+							while true
+							do
+								unset static_lib_dirs
+								for static_lib_dir in $static_lib_dir_list
+								do
+									static_lib_dirs_num=`expr ${#static_lib_dirs[*]} / 2 + 1`
+									static_lib_dirs[${#static_lib_dirs[*]}]=$static_lib_dirs_num
+									static_lib_dirs[${#static_lib_dirs[*]}]=$static_lib_dir
+								done
+
+								static_lib_dirs_num=`expr ${#static_lib_dirs[*]} / 2`
+								OPTION=$(whiptail --title "Menu Dialog" --menu "查看静态库目录" 15 60 $static_lib_dirs_num "${static_lib_dirs[@]}" 3>&1 1>&2 2>&3)
+
+								exitstatus=$?
+								if [ $exitstatus != 0 ]; then
+									echo "You chose Cancel."
+									break
+								fi
+							done
 						;;
-					"删除静态库目录")
-						for static_lib_file in $static_lib_dir_list
-						do
-							static_lib_file_num=`expr ${#choose_compiler[*]} / 2`
-							static_lib_file[${#static_lib_file[*]}]=$static_lib_file_num
-							static_lib_file[${#static_lib_file[*]}]=$static_lib_file
-							option_state[${#option_state[*]}]=OFF
-						done
-
-						static_lib_file_num=`expr ${#static_lib_file[*]} / 2`
-						OPTION=$(whiptail --title "Menu Dialog" --checklist "删除静态库目录" 15 60 $static_lib_file_num "${static_lib_file[@]}" ${option_state[@]} 3>&1 1>&2 2>&3)
-
-						exitstatus=$?
-						if [ $exitstatus != 0 ]; then
-							echo "You chose Cancel."
-							break
-						fi
-
-						for del_file in $OPTION
-						do
-							static_lib_dir_list=`echo $static_lib_dir_list | sed "s/$del_file//g"`
-						done
-						;;
-					"静态库目录列表")
-						for static_lib_file in $static_lib_dir_list
-						do
-							static_lib_file_num=`expr ${#choose_compiler[*]} / 2`
-							static_lib_file[${#static_lib_file[*]}]=$static_lib_file_num
-							static_lib_file[${#static_lib_file[*]}]=$static_lib_file
-						done
-
-						static_lib_file_num=`expr ${#static_lib_file[*]} / 2`
-						OPTION=$(whiptail --title "Menu Dialog" --menu "删除静态库目录" 15 60 $static_lib_file_num "${static_lib_file[@]}" 3>&1 1>&2 2>&3)
-
-						exitstatus=$?
-						if [ $exitstatus != 0 ]; then
-							echo "You chose Cancel."
-							break
-						fi
-						;;
-				esac
+					esac
+				done
 				;;
 			">头文件目录列表")
-				OPTION=$(whiptail --title "Menu Dialog" --menu "头文件目录" 15 60 2 \
-				"1" "添加头文件目录" \
-				"2" "删除头文件目录" \
-				"3" "头文件目录列表" \
-				3>&1 1>&2 2>&3)
+				while true
+				do
+					OPTION=$(whiptail --title "Menu Dialog" --menu "头文件目录" 15 60 3 \
+					"1" "添加头文件目录" \
+					"2" "删除头文件目录" \
+					"3" "头文件目录列表" \
+					3>&1 1>&2 2>&3)
 
-				exitstatus=$?
-				if [ $exitstatus != 0 ]; then
-					echo "You chose Cancel."
-					break
-				fi
+					exitstatus=$?
+					if [ $exitstatus != 0 ]; then
+						echo "You chose Cancel."
+						break
+					fi
 
-				case $OPTION in
-					"添加头文件目录")
-						new_header_dir=$(whiptail --title "添加头文件目录" --inputbox "" 10 60 3>&1 1>&2 2>&3)
+					case $OPTION in
+						"1")
+							new_header_dir=$(whiptail --title "添加头文件目录" --inputbox "添加头文件目录" 10 60 3>&1 1>&2 2>&3)
 
-						exitstatus=$?
-						if [ $exitstatus != 0 ]; then
-							echo "You chose Cancel."
-							break
-						fi
-						
-						header_file_path_list=$header_file_path_list" "$new_header_dir
+							exitstatus=$?
+							if [ $exitstatus == 0 ]; then
+								header_dir_path_list=$header_dir_path_list" "$new_header_dir
+							fi
 						;;
-					"删除头文件目录")
-						for header_file in $header_file_path_list
-						do
-							header_files_num=`expr ${#choose_compiler[*]} / 2`
-							header_files[${#header_files[*]}]=$header_files_num
-							header_files[${#header_files[*]}]=$header_file
-							option_state[${#option_state[*]}]=OFF
-						done
+						"2")
+							while true 
+							do
+								unset header_dirs
+								for header_dir in $header_dir_path_list
+								do
+									header_dirs_num=`expr ${#header_dirs[*]} / 2 + 1`
+									header_dirs[${#header_dirs[*]}]=$header_dirs_num
+									header_dirs[${#header_dirs[*]}]=$header_dir
+								done
 
-						header_files_num=`expr ${#header_files[*]} / 2`
-						OPTION=$(whiptail --title "Menu Dialog" --checklist "删除头文件目录" 15 60 $header_files_num "${header_files[@]}" ${option_state[@]} 3>&1 1>&2 2>&3)
+								header_dirs_num=`expr ${#header_dirs[*]} / 2`
+								OPTION=$(whiptail --title "Menu Dialog" --menu "删除头文件目录" 15 60 $header_dirs_num "${header_dirs[@]}" 3>&1 1>&2 2>&3)
 
-						exitstatus=$?
-						if [ $exitstatus != 0 ]; then
-							echo "You chose Cancel."
-							break
-						fi
+								exitstatus=$?
+								if [ $exitstatus != 0 ]; then
+									echo "You chose Cancel."
+									break
+								fi
 
-						for del_head_file in $OPTION
-						do
-							header_file_path_list=`echo $header_file_path_list | sed "s/$del_head_file//g"`
-						done
+								index=$[$OPTION * 2 - 1]
+								del_header_dir=${header_dirs[$index]}
+
+								if (whiptail --title "Yes/No Box" --yesno "确定要移除 $del_header_dir" 10 60) then
+									header_dir_path_list=`echo $header_dir_path_list | sed "s#$del_header_dir##g"`
+								else
+									echo "You chose No. Exit status was $?."
+								fi
+							done 
+							;;
+						"3")
+							while true 
+							do
+								unset header_dirs
+								for header_dir in $header_dir_path_list
+								do
+									header_dirs_num=`expr ${#header_dirs[*]} / 2 + 1`
+									header_dirs[${#header_dirs[*]}]=$header_dirs_num
+									header_dirs[${#header_dirs[*]}]=$header_dir
+								done
+
+								header_dirs_num=`expr ${#header_dirs[*]} / 2`
+								OPTION=$(whiptail --title "Menu Dialog" --menu "查看头文件目录" 15 60 $header_dirs_num "${header_dirs[@]}" 3>&1 1>&2 2>&3)
+
+								exitstatus=$?
+								if [ $exitstatus != 0 ]; then
+									echo "You chose Cancel."
+									break
+								fi
+							done 
 						;;
-					"头文件目录列表")
-						for header_file in $header_file_path_list
-						do
-							header_files_num=`expr ${#choose_compiler[*]} / 2`
-							header_files[${#header_files[*]}]=$header_files_num
-							header_files[${#header_files[*]}]=$header_file
-							option_state[${#option_state[*]}]=OFF
-						done
-
-						header_files_num=`expr ${#header_files[*]} / 2`
-						OPTION=$(whiptail --title "Menu Dialog" --menu "头文件目录" 15 60 $header_files_num "${header_files[@]}" 3>&1 1>&2 2>&3)
-
-						exitstatus=$?
-						if [ $exitstatus != 0 ]; then
-							echo "You chose Cancel."
-							break
-						fi
-						;;
-				esac
+					esac
+				done
 				;;
 			">当前程序入口文件: ")
+				echo "$program_entry_file_list"
+				unset program_entry_files #清空数组元素，防止下次循环回来数组元素添加出现叠加现象
 				for entry_file in $program_entry_file_list
 				do
-					program_entry_files_num=`expr ${#choose_compiler[*]} / 2`
-					program_entry_files[${#program_entry_files[*]}]=$program_entry_file_num
+					program_entry_files_num=`expr ${#program_entry_files[*]} / 2 + 1`
+					program_entry_files[${#program_entry_files[*]}]=$program_entry_files_num
 					program_entry_files[${#program_entry_files[*]}]=$entry_file
+					echo "${program_entry_files[@]}"
 				done
 
 				program_entry_files_num=`expr ${#program_entry_files[*]} / 2`
-				OPTION=$(whiptail --title "Menu Dialog" --menu "选择程序入口文件" 15 60 $compile_num "${program_entry_files[@]}"  3>&1 1>&2 2>&3)
+				OPTION=$(whiptail --title "Menu Dialog" --menu "选择程序入口文件" 15 60 $program_entry_files_num ${program_entry_files[@]}  3>&1 1>&2 2>&3)
 
 				exitstatus=$?
 				if [ $exitstatus != 0 ]; then
@@ -314,7 +427,8 @@ function config_project_config()
 					break
 				fi
 
-				current_program_entry_file=$OPTION
+				index=$[$OPTION * 2 - 1]
+				current_program_entry_file=${program_entry_files[$index]}
 				;;
 			">当前生成文件类型: ")
 				OPTION=$(whiptail --title "Menu Dialog" --menu "生成文件类型" 15 60 3 \
@@ -341,7 +455,7 @@ function config_project_config()
 				unset choose_compiler #清空数组元素，防止下次循环回来数组元素添加出现叠加现象
 				for compile in $compiler_list
 				do
-					compile_num=`expr ${#choose_compiler[*]} / 2 `
+					compile_num=`expr ${#choose_compiler[*]} / 2 + 1`
 					choose_compiler[${#choose_compiler[*]}]=$compile_num
 					choose_compiler[${#choose_compiler[*]}]=$compile
 				done
@@ -354,7 +468,7 @@ function config_project_config()
 					echo "You chose Cancel."
 					break
 				fi
-				index=$[$OPTION * 2 + 1]
+				index=$[$OPTION * 2 - 1]
 				current_compiler=${choose_compiler[$index]}
 				;;
 			">编译方式: ")
